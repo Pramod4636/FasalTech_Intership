7eff6b479528409f4ffbb1e22d899b25617bf88c
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-05 22:45:44 +0530

    react components

---
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <>
+    <h2>Let's get started !</h2>
+    <p>This is also visible </p>
+    <ExpenseItem/>
+    </>
+  );
+}
+
+export default App;
---
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <h2>Epense item ! </h2>
+    </div>
+  )
+}
+
+export default ExpenseItem
