53942c8519dd86dc84afcb4d3bd0651587116126
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-05 21:02:15 +0530

    classss

--- Learning_React/Section2_newJs/file1.js
+++ Learning_React/Section2_newJs/file1.js
@@ -14,3 +14,28 @@ console.log(multipy(3));
 console.log(abc(35,4));
 
 
+class Human {
+
+    constructor() {
+        this.gender = 'male';
+
+    }
+    printGender() {
+        console.log(this.gender);    
+    }
+}
+
+
+class Person extends Human{
+    constructor(){
+        super();
+        this.name = 'Max ';
+    }
+    printMyName(){
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
