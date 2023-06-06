6e1f89cda30d62801c69389818f9f14de75edb53
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-06 12:19:03 +0530

    variable adding in JSX

--- Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
@@ -2,15 +2,18 @@ import React from 'react'
 import './f1.css';
 
 const ExpenseItem = () => {
-  return (
-    <div className = "expense-item">
-      <div >March 28th 2021 </div> 
+  const expenseDate = new Date(2021,2,28);
+  const expenseTitle = 'Car-Insurance';
+  const expenseAmount = 294.67;
+
+  return ( <div className = "expense-item">
+      <div >{expenseDate.toISOString()} </div> 
       <div className = "expense-item__description"> 
-        <h2> Car Insurance </h2> 
-        <div className="expense-item__price"> $294.67 </div>
+        <h2> {expenseTitle} </h2>
+        <div className="expense-item__price"> {expenseAmount} </div>
       </div>
-    </div>
-  )
+    </div> );
+  
 }
 
 export default ExpenseItem
