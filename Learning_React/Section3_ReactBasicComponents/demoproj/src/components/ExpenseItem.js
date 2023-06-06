2bf9893593652a286e62f801853159169ceaa9fa
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-06 14:25:34 +0530

    component making using another component

--- Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './f1.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
    const month = props.date.toLocaleString('en-US' , {month : 'long'}); //toLocalString('en-US',{month :'long'});
@@ -9,11 +10,7 @@ const ExpenseItem = (props) => {
 
   return ( <div className = "expense-item">
      
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date = {props.date} />
       <div className = "expense-item__description"> 
         <h2> {props.title} </h2>
         <div className="expense-item__price"> {props.price} </div>
