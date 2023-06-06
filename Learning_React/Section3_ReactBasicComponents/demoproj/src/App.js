1ea91c9731acd31628b05645729238e9c5fb24ef
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-06 12:57:06 +0530

    props data passing to components

--- Learning_React/Section3_ReactBasicComponents/demoproj/src/App.js
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/App.js
@@ -1,12 +1,39 @@
 
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
+const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+
 function App() {
   return (
     <>
     <h2>Let's get started !</h2>
     <p>This is also visible </p>
-    <ExpenseItem/>
+    
+    <ExpenseItem date = {expenses[0].date} title = {expenses[0].title} price = {expenses[0].amount} />
+    <ExpenseItem date = {expenses[1].date} title = {expenses[1].title} price = {expenses[1].amount} />
+    <ExpenseItem date = {expenses[2].date} title = {expenses[2].title} price = {expenses[2].amount} />
+    
     </>
   );
 }
