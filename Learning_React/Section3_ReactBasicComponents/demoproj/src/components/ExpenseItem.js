db1bec2625e3d205fbdf54e7afca90126ee31b1d
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-06 11:57:04 +0530

    css adding in react componentat

--- Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './f1.css';
 
 const ExpenseItem = () => {
   return (
-    <div>
-      <h2>Epense item ! </h2>
+    <div className = "expense-item">
+      <div >March 28th 2021 </div> 
+      <div className = "expense-item__description"> 
+        <h2> Car Insurance </h2> 
+        <div className="expense-item__price"> $294.67 </div>
+      </div>
     </div>
   )
 }
