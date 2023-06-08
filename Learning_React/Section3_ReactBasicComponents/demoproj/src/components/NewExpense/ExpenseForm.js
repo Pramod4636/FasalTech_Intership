88795e4fb9d16412cc962ccb21e5a3d5f592aaa8
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-08 07:56:20 +0530

    form input all store in one object

--- Learning_React/Section3_ReactBasicComponents/demoproj/src/components/NewExpense/ExpenseForm.js
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/components/NewExpense/ExpenseForm.js
@@ -2,22 +2,26 @@ import React ,{useState} from 'react'
 import './f2.css';
 
 const ExpenseForm = () => {
-
-    const [enteredTitle,setEnteredTitle] = useState('');
-    const [enteredAmount,setEnteredAmount] = useState('');
-    const [enteredDate,setEnteredDate] =useState('');
+   
+   const [userInput,setUserInput] = useState({
+    enteredTitle :'',
+    enteredAmount : '',
+    enteredDate : ''
+   });    
 
    const titleChangeHandler = (event) =>{
-        setEnteredTitle(event.target.value);
-        console.log(enteredTitle);
+        setUserInput({...userInput,"enteredTitle" : event.target.value});
+        console.log(userInput);
    }
 
    const amountChangeHandler = (event)=>{
-    setEnteredAmount(event.target.value);
+    setUserInput({...userInput,"enteredAmount" : event.target.value});
+        
    }
 
    const dateChangeHandler = (event) =>{
-    setEnteredDate(event.target.value);
+  
+    setUserInput({...userInput,"enteredDate" : event.target.value});
    }
 
   return (
--- Learning_React/Section3_ReactBasicComponents/demoproj/src/components/NewExpense/NewExpense.js
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './f1.css';
 import ExpenseForm from './ExpenseForm';
+//Css add 
 const NewExpense = () => {
   return (
-    <div className='new-expense'> //Css add 
+    <div className='new-expense'> 
         <ExpenseForm />
     </div>
   )
