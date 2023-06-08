df1578f102485f187fa37de8227ec8dd96b1a192
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-08 06:38:00 +0530

    Event listen and take action  button click and do thing

--- Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
+++ Learning_React/Section3_ReactBasicComponents/demoproj/src/components/ExpenseItem.js
@@ -4,14 +4,17 @@ import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
   
+  const clickHandler = () => {
+    alert("Changing Title ");
+  }
 
   return ( <div className = "expense-item">
-     
       <ExpenseDate date = {props.date} />
       <div className = "expense-item__description"> 
         <h2> {props.title} </h2>
         <div className="expense-item__price"> {props.price} </div>
       </div>
+      <button onClick = {()=> clickHandler()} >Change Title</button>
     </div> );
   
 }
